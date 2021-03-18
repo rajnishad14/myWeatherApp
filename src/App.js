5ede0a2c85b7d3ec2fc0db98b7eafa47f18be1b7
rajnishad14 <raj.nishad14@gmail.com>
2021-03-19 00:39:12 +0530

    ussed pritteier

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import { useEffect, useState } from 'react';
-import Display from "./components/Display"
-import './App.css';
-import { Dimmer, Loader } from 'semantic-ui-react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { useEffect, useState } from "react";
+import Display from "./components/Display";
+import "./App.css";
+import { Dimmer, Loader } from "semantic-ui-react";
+import { Container } from "react-bootstrap";
 
 function App() {
-
-  // const [cityName, setCityName] = useState("");
-  // const [city, setCity] = useState("");
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
-  // const handleChange = (event) => {
-  //   setCityName(event.target.value);
-  // }
-  // const handleClick = () => {
-  //   setCity(cityName)
-  // }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
-      await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=317623db6367401294fc05a548e9ebf8`)
-        .then(res => res.json())
-        .then(result => {
-          setData(result)
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
         });
-    }
-    fetchData();
-  }, //eslint-disable-next-line 
-    [lat, long])
-
-
+        await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=317623db6367401294fc05a548e9ebf8`
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            setData(result);
+          });
+      };
+      fetchData();
+    }, //eslint-disable-next-line
+    [lat, long]
+  );
 
   return (
-    <div className='App'>
+    <div className="App">
       <Container>
-        {(typeof data.main != 'undefined') ? (
+        {typeof data.main != "undefined" ? (
           <Display weatherData={data} />
         ) : (
           <Dimmer active>
